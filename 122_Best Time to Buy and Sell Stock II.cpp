6ea373f506066b45a272f89715a48b6c8fe7adbd
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2022-09-10 12:18:34 +0530

    Update 122_Best Time to Buy and Sell Stock II.cpp

--- 122_Best Time to Buy and Sell Stock II.cpp
+++ 122_Best Time to Buy and Sell Stock II.cpp
@@ -29,8 +29,8 @@ Explanation: There is no way to make a positive profit, so we never buy the stoc
  
 Constraints:
 
-1 <= prices.length <= 3 * 104
-0 <= prices[i] <= 104
+1 <= prices.length <= 3 * 10^4
+0 <= prices[i] <= 10^4
 
 */
 
