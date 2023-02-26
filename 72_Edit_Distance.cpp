2ba61fc742d366c15f9ae0eb3691ee0120881718
Yash Gaherwar <72980929+yashgaherwar@users.noreply.github.com>
2023-02-26 19:22:00 +0530

    Create 72_Edit_Distance.cpp

---
+++ 72_Edit_Distance.cpp
@@ -0,0 +1,70 @@
+/*
+
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+ 
+Constraints:
+
+0 <= word1.length, word2.length <= 500
+word1 and word2 consist of lowercase English letters.
+
+*/
+
+/* Solution */
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        int dp[n+1][m+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                }
+            }
+        }
+
+        return dp[n][m];
+
+    }
+};
